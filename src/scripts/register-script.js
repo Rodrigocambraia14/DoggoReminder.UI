3baa8b98e75afa2190ed65cb2e7e47dc1b9147b8
RodrigoCambraia14 <rodrigocambraia.jr@gmail.com>
2024-03-27 16:01:39 -0300

    cadastro

---
+++ src/scripts/register-script.js
@@ -0,0 +1,54 @@
+// main.js
+import { API_URL } from '../utils/constants.js';
+import { validateEmail } from '../utils/validations.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('register-form');
+
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent form submission
+        
+        const emailInput = document.getElementById('register-email');
+        const nameInput = document.getElementById('register-name');
+        const passwordInput = document.getElementById('register-password');
+
+        if (!validateEmail(emailInput.value)) {
+            alert('Por favor, insira um e-mail válido.');
+            return;
+        }
+
+        if (!emailInput.value || !passwordInput.value || !nameInput.value) {
+            alert('Por favor, preencha todos os campos.');
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('email', emailInput.value);
+        formData.append('name', nameInput.value);
+        formData.append('password', passwordInput.value);
+
+        fetch(API_URL, {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(response.Error);
+            }
+        })
+        .then(data => {
+            // Handle successful login, e.g., redirect to dashboard
+            console.log('Register successful:', data);
+            // window.location.href = '/dashboard.html'; // Redirect to dashboard
+        })
+        .catch(error => {
+            // Handle login error
+            console.error('Register error:', error);
+            alert('Falha ao realizar cadastro, verifique seus dados e tente novamente.');
+        });
+    });
+
+    
+});
